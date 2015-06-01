da8d8ae29ef81db39ca2d6af439d88f3d6ecfebd
Johan Olofsson <johan.olofsson@yolean.com>
2015-06-01 14:42:06 +0200

    Found indentation problems while testing on Windows Server.

--- mssql_backend/mssql_backend.py
+++ mssql_backend/mssql_backend.py
@@ -33,13 +33,13 @@ except ImportError:
 
 # Mapping from "abstract" SQL types to DB-specific types
 _type_map = {
-    'int64': 'bigint',
-    'text': 'nvarchar(512)',
+	'int64': 'bigint',
+	'text': 'nvarchar(512)',
 }
 
 # TODO: You cannot use MS Access because column name 'value' can seems not use via odbc.
 _column_map = {
-    'key': '"key"',
+	'key': '"key"',
 #    'value': '"value"'
 }
 
@@ -191,145 +191,145 @@ class MSSQLConnection(ConnectionBase, ConnectionWrapper):
 
 
 def _to_sql(table):
-    sql = ["CREATE TABLE %s (" % table.name]
-    coldefs = []
-    for column in table.columns:
-        column.name = _column_map.get(column.name, column.name)
-        ctype = column.type.lower()
-        ctype = _type_map.get(ctype, ctype)
-        #  for SQL Server, patch for "enum" table, value is not text, use int instead.
-        if table.name == 'enum' and column.name == 'value':
-            ctype = 'int'
-        if (table.name, column.name) in [
-                ('wiki', 'text'),
-                ('report', 'query'),
-                ('report', 'description'),
-                ('milestone', 'description'),
-                ('version', 'description'),
-            ]:
-            ctype = 'nvarchar(MAX)'
-        if (table.name, column.name) in [
-                ('ticket', 'description'),
-                ('ticket_change', 'oldvalue'),
-                ('ticket_change', 'newvalue'),
-                ('ticket_custom', 'value'),
-                ('session_attribute', 'value')
-            ]:
-            ctype = 'nvarchar(4000)'
+	sql = ["CREATE TABLE %s (" % table.name]
+	coldefs = []
+	for column in table.columns:
+		column.name = _column_map.get(column.name, column.name)
+		ctype = column.type.lower()
+		ctype = _type_map.get(ctype, ctype)
+		#  for SQL Server, patch for "enum" table, value is not text, use int instead.
+		if table.name == 'enum' and column.name == 'value':
+			ctype = 'int'
+		if (table.name, column.name) in [
+				('wiki', 'text'),
+				('report', 'query'),
+				('report', 'description'),
+				('milestone', 'description'),
+				('version', 'description'),
+			]:
+			ctype = 'nvarchar(MAX)'
+		if (table.name, column.name) in [
+				('ticket', 'description'),
+				('ticket_change', 'oldvalue'),
+				('ticket_change', 'newvalue'),
+				('ticket_custom', 'value'),
+				('session_attribute', 'value')
+			]:
+			ctype = 'nvarchar(4000)'
 
 # I'm using SQL Userver 2012 Express
-        if column.auto_increment:
-            ctype = 'INT IDENTITY NOT NULL'  # SQL Server Style
+		if column.auto_increment:
+			ctype = 'INT IDENTITY NOT NULL'  # SQL Server Style
 #            ctype = 'INT UNSIGNED NOT NULL AUTO_INCREMENT'  # MySQL Style
 #            ctype = 'SERIAL'  # PGSQL Style
 #            ctype = "integer constraint P_%s PRIMARY KEY" % table.name  # SQLite Style
-        else:
+		else:
 #            if column.name in table.key or any([column.name in index.columns for index in table.indices]):
 #                ctype = {'ntext': 'nvarchar(255)'}.get(ctype, ctype)  # SQL Server cannot use text as PK
-            if len(table.key) == 1 and column.name in table.key:
-                ctype += " constraint P_%s PRIMARY KEY" % table.name
-        coldefs.append("    %s %s" % (column.name, ctype))
-    if len(table.key) > 1:
-        coldefs.append("    UNIQUE (%s)" % ','.join(table.key))
-    sql.append(',\n'.join(coldefs) + '\n);')
-    yield '\n'.join(sql)
-    for index in table.indices:
-        type_ = ('INDEX', 'UNIQUE INDEX')[index.unique]
-        yield "CREATE %s %s_%s_idx ON %s (%s);" % (type_, table.name,
-              '_'.join(index.columns), table.name, ','.join(index.columns))
+			if len(table.key) == 1 and column.name in table.key:
+				ctype += " constraint P_%s PRIMARY KEY" % table.name
+		coldefs.append("    %s %s" % (column.name, ctype))
+	if len(table.key) > 1:
+		coldefs.append("    UNIQUE (%s)" % ','.join(table.key))
+	sql.append(',\n'.join(coldefs) + '\n);')
+	yield '\n'.join(sql)
+	for index in table.indices:
+		type_ = ('INDEX', 'UNIQUE INDEX')[index.unique]
+		yield "CREATE %s %s_%s_idx ON %s (%s);" % (type_, table.name,
+			  '_'.join(index.columns), table.name, ','.join(index.columns))
 
 
 class SQLServerCursor(object):
 
-    def __init__(self, cursor, log=None):
-        self.cursor = cursor
-        self.log = log
+	def __init__(self, cursor, log=None):
+		self.cursor = cursor
+		self.log = log
 
-    def __getattr__(self, name):
-        return getattr(self.cursor, name)
+	def __getattr__(self, name):
+		return getattr(self.cursor, name)
 
-    def __iter__(self):
-        while True:
-            row = self.cursor.fetchone()
-            if not row:
-                return
-            yield row
+	def __iter__(self):
+		while True:
+			row = self.cursor.fetchone()
+			if not row:
+				return
+			yield row
 
-    def execute(self, sql, args=None):
-        if args:
-            sql = sql % (('%s',) * len(args))
+	def execute(self, sql, args=None):
+		if args:
+			sql = sql % (('%s',) * len(args))
 
-        # replace __column__ IS NULL -> COALESCE(__column__, '') after ORDER BY
-        match = re_order_by.search(sql)
-        if match:
-            end = match.end()
-            for match in reversed([match for match in re_isnull.finditer(sql[end:])]):
-                replacement = "COALESCE(%s,'')" % match.group(1)
-                sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
+		# replace __column__ IS NULL -> COALESCE(__column__, '') after ORDER BY
+		match = re_order_by.search(sql)
+		if match:
+			end = match.end()
+			for match in reversed([match for match in re_isnull.finditer(sql[end:])]):
+				replacement = "COALESCE(%s,'')" % match.group(1)
+				sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
 
-        # replace __column__ = %s -> CASE __column__ WHEN %s THEN '0' ELSE '1' END after ORDER BY
-        match = re_order_by.search(sql)
-        if match:
-            end = match.end()
-            for match in reversed([match for match in re_equal.finditer(sql[end:])]):
-                replacement = "CASE %s WHEN %s THEN '0' ELSE '1' END" % (match.group(1), match.group(2))
-                sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
-            for match in reversed([match for match in re_coalesce_equal.finditer(sql[end:])]):
-                replacement = "CASE %s WHEN %s THEN '0' ELSE '1' END" % (match.group(1), match.group(2))
-                sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
+		# replace __column__ = %s -> CASE __column__ WHEN %s THEN '0' ELSE '1' END after ORDER BY
+		match = re_order_by.search(sql)
+		if match:
+			end = match.end()
+			for match in reversed([match for match in re_equal.finditer(sql[end:])]):
+				replacement = "CASE %s WHEN %s THEN '0' ELSE '1' END" % (match.group(1), match.group(2))
+				sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
+			for match in reversed([match for match in re_coalesce_equal.finditer(sql[end:])]):
+				replacement = "CASE %s WHEN %s THEN '0' ELSE '1' END" % (match.group(1), match.group(2))
+				sql = sql[:end + match.start()] + replacement + sql[end + match.end():]
 
-        # trim duplicated columns after ORDER BY
-        match = re_order_by.search(sql)
-        if match:
-            end = match.end()
-            match = re.search("'([a-z]+)'", sql[end:])
-            if match:
-                column_name = match.group(1)
-                re_columns = re.compile("([a-z]+.)?%s,?" % column_name)
-                order_by = ' '.join([column for column in match.string.split(' ') if not re_columns.match(column)])
-                self.log.debug(order_by)
-                sql = sql[:end] + order_by
+		# trim duplicated columns after ORDER BY
+		match = re_order_by.search(sql)
+		if match:
+			end = match.end()
+			match = re.search("'([a-z]+)'", sql[end:])
+			if match:
+				column_name = match.group(1)
+				re_columns = re.compile("([a-z]+.)?%s,?" % column_name)
+				order_by = ' '.join([column for column in match.string.split(' ') if not re_columns.match(column)])
+				self.log.debug(order_by)
+				sql = sql[:end] + order_by
 
-        # transform LIMIT clause
-        match = re_limit.search(sql)
-        if match:
-            limit = match.group(1)
-            offset = match.group(3)
-            if not offset:
-                # LIMIT n (without OFFSET) -> SELECT TOP n
-                sql = match.string[:match.start()].replace("SELECT", "SELECT TOP %s" % limit)
-            else:
-                # LIMIT n OFFSET m -> OFFSET m ROWS FETCH NEXT n ROWS ONLY
-                sql = match.string[:match.start()] + " OFFSET %s ROWS FETCH NEXT %s ROWS ONLY" % (offset, limit)
+		# transform LIMIT clause
+		match = re_limit.search(sql)
+		if match:
+			limit = match.group(1)
+			offset = match.group(3)
+			if not offset:
+				# LIMIT n (without OFFSET) -> SELECT TOP n
+				sql = match.string[:match.start()].replace("SELECT", "SELECT TOP %s" % limit)
+			else:
+				# LIMIT n OFFSET m -> OFFSET m ROWS FETCH NEXT n ROWS ONLY
+				sql = match.string[:match.start()] + " OFFSET %s ROWS FETCH NEXT %s ROWS ONLY" % (offset, limit)
 #                match = re_where.search(sql)
 #                sql = match.string[:match.end()] + 'ROW_NUMBER() > %s, ' % limit + match.string[match.end():]
-        # avoid error in "order by" in sub query
-        # TODO: decide count of lines
-        else:
-            for match in reversed([match for match in re_select.finditer(sql) if match.group(2) == None]):
-                sql = sql[:match.end()] + ' TOP 1000' + sql[match.end():]
-        try:
-            if self.log:  # See [trac] debug_sql in trac.ini
-                self.log.debug(sql)
-                self.log.debug(args)
-                if args:
-			self.cursor.execute(sql, tuple(args))
+		# avoid error in "order by" in sub query
+		# TODO: decide count of lines
 		else:
-			self.cursor.execute(sql, ())
-        except:
-            self.cnx.rollback()
-            raise
+			for match in reversed([match for match in re_select.finditer(sql) if match.group(2) == None]):
+				sql = sql[:match.end()] + ' TOP 1000' + sql[match.end():]
+		try:
+			if self.log:  # See [trac] debug_sql in trac.ini
+				self.log.debug(sql)
+				self.log.debug(args)
+			if args:
+				self.cursor.execute(sql, tuple(args))
+			else:
+				self.cursor.execute(sql, ())
+		except:
+			self.cnx.rollback()
+			raise
 
-    def executemany(self, sql, args):
-        if not args:
-            return
-        sql = sql % (('%s',) * len(args[0]))
-        try:
-            if self.log:  # See [trac] debug_sql in trac.ini
-                self.log.debug(sql)
-                self.log.debug(args)
-            self.cursor.executemany(sql, args)
-        except:
-            self.cnx.rollback()
-            raise
+	def executemany(self, sql, args):
+		if not args:
+			return
+		sql = sql % (('%s',) * len(args[0]))
+		try:
+			if self.log:  # See [trac] debug_sql in trac.ini
+				self.log.debug(sql)
+				self.log.debug(args)
+			self.cursor.executemany(sql, args)
+		except:
+			self.cnx.rollback()
+			raise
 
